325c5a19d29089a1de6689b98328892a74ddefb1
FinlayS <fsgm01@gmail.com>
2018-10-17 21:16:02 +0100

    S6 L54 TEsting asyncronous code

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -10,56 +10,51 @@ it('should add two numbers', () => {
 
 });
 
+it('should asyncAdd two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+    })
+});
+
 it('should square a number', () => {
     var res = utils.square(3)
 
     expect(res).toBe(9).toBeA('number')
 });
 
-
+it('should asyncAdd square a number', (done) => {
+    utils.asyncSquare(3, (sum) => {
+        expect(sum).toBe(9).toBeA('number');
+        done();
+    })
+});
 
 it('Should have first name and last name with proper values', () => {
 
     var user = {location: 'home', age: '54'};
     var res = utils.setName(user, 'finlay simpson');
 
-    // console.log(user)
-    // console.log(res)
     expect(user).toEqual(res);
 
     expect(res).toInclude({
         firstName:'finlay',
         lastName: 'simpson'
         })
-
 });
 
-// should verify first and last names are set
+it ('A whole bunch of things...Should..', () => {
+     expect(12).toNotBe(13);
 
-// create a user object '
-//like with first and last names age locatoion
+     expect({name: 'Finlay'}).toEqual({name: 'Finlay'});
 
-// pass into set name methods
+    expect([2, 3, 4]).toInclude(2);
 
-// then a string with first  and last name
-
-// asset include first & last with proper values
-
-
-
-
-// it ('A whole bunch of things...Should..', () => {
-//      expect(12).toNotBe(12);
-//
-//      expect({name: 'Finlay'}).toEqual({name: 'Finlay'});
-//
-//     expect([2, 3, 4]).toInclude(2);
-//
-//     expect({
-//         name: 'Finlay',
-//         age: '23',
-//         home: 'Yes',
-//     }).toInclude({
-//         age: '23'
-//     })
-// });
+    expect({
+        name: 'Finlay',
+        age: '23',
+        home: 'Yes',
+    }).toInclude({
+        age: '23'
+    })
+});
