a0b22b9db70a7919e7e4bb069398bff9adcd13d0
Finlay <fsgm01@gmail.com>
2018-10-23 14:44:14 +0100

    half done excercise for L56

--- server/server.test.js
+++ server/server.test.js
@@ -1,10 +1,31 @@
 const request = require('supertest');
+const expect = require('expect');
 
 var app = require('./server').app;
+var app2 = require('./server').app2;
 
-it('should return hello world', (done) => {
-  request(app)
-      .get('/')
-      .expect('Hello worlds')
+
+// it('should return hello world', (done) => {
+//   request(app)
+//       .get('/')
+//       .expect(404)
+//       .expect((res) => {
+//       expect(res.body).toInclude({
+//         name: 'Todo App v1.0'
+//       })
+//
+//       })
+//       .end(done);
+// })
+
+it('should return array', (done) => {
+  request(app2)
+      .get('/users')
+      //.expect(404)
+      .expect((res) => {
+
+        expect(res.body).toBe.arrayContaining('twenty')
+
+      })
       .end(done);
 })
